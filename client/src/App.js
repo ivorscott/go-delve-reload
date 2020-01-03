49d2a62456afb1e9d8b052ea3d34059fb000bf61
Ivor <ivor@devpie.io>
2020-01-04 00:46:49 +0100

    feat: add client app

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await fetch(`https://api.local/products`);
+      const json = await response.json();
+      this.setState(() => ({ products: json }));
+    } catch (error) {}
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <footer style={{ textAlign: "left" }}>
+          <pre>
+            <code>
+              {products.length > 0 && JSON.stringify(products, null, 4)}
+            </code>
+          </pre>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
